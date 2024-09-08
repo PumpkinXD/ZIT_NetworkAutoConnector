00078c0a2ad7af304c8f38490f937a0e6c63217e
PumpkinXD <54535387+PumpkinXD@users.noreply.github.com>
2024-09-08 23:58:14 +0800

    init

---
+++ utf8string_handle.cpp
@@ -0,0 +1,39 @@
+#include "utf8string_handle.hpp"
+
+
+
+
+
+namespace io_github_pumpkinxd_ZitNetworkAutoConnector {
+std::vector<std::string> splitString(const std::string &in)
+{
+    // std::setlocale(LC_ALL, "");
+    std::mbstate_t mb = std::mbstate_t();
+    std::vector<std::string> ret;
+    const char* ptr = in.data();
+    const char* const end = ptr + in.size();
+    
+    while (ptr < end)
+    {
+        std::size_t next = std::mbrlen(ptr, end - ptr, &mb);
+        if (next == (std::size_t)-1)
+            throw std::runtime_error("strlen_mb(): conversion error");
+        if (next == (std::size_t)-2)
+            throw std::runtime_error("strlen_mb(): incomplete multibyte sequence");
+        if (next == 0)
+            break;
+        
+        ret.push_back(std::string(ptr, next));
+        ptr += next;
+    }
+    
+    return ret;
+}
+std::string extractQueryString(const std::string& url) {
+    size_t start = url.find('?');
+    if (start == std::string::npos) {
+        return "";
+    }
+    return url.substr(start + 1);
+}
+}
---
+++ utf8string_handle.hpp
@@ -0,0 +1,24 @@
+#ifndef UTF8STRING_HANDLE
+#define UTF8STRING_HANDLE
+#include <cstdlib>
+#include <cstring>
+#include <cuchar>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace io_github_pumpkinxd_ZitNetworkAutoConnector {
+
+std::vector<std::string> splitString(const std::string &in);
+std::string extractQueryString(const std::string& url);
+} // namespace io_github_pumpkinxd_ZitNetworkAutoConnector
+
+
+
+
+
+
+
+
+
+#endif /* UTF8STRING_HANDLE */
